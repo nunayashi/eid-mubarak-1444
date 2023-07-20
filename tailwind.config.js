4372489e9c5f728c6ddccecf48d4dc223e141f7b
nunayashi <nurmohamad12@gmail.com>
2023-07-20 21:38:26 +0700

    revisi

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,6 @@ module.exports = {
   content: ['index.html'],
   theme: {
     extend: {
-      backgroundImage: (theme) => ({
-        'hero-lg': "url('/img/hero-lg.jpg')",
-        'hero-sm': "url('/img/hero-sm.jpg')",
-    }),
       fontFamily: {
         almarai: ['Almarai'],
         poppins: ['Poppins']
